e92ba3001fa07885341d0d9113e25e24e0c50d56
Shaun Buckley <shaun.c.buckley@gmail.com>
2014-06-08 17:57:59 -0700

    Add pins/users association

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about' # creates about_path
   get 'team' => 'pages#team' # creates team_path
-  get 'pins' => 'pages#pins' # creates pins_path
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
